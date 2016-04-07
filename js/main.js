e2f4de4132ec629dde06492e9fa9ec8228d4825b
TryBr <avulsed.jb@gmail.com>
2016-04-07 21:55:45 +0500

    Сделана шкала годов. Контакты. Футер.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 $( "#tabs" ).tabs();
 $( "#tabs2" ).tabs();
+$( "#timeline" ).tabs();
 $(document).ready(function() {
  
   $("#owl-demo").owlCarousel({
  
       navigation : true, // Show next and prev buttons
-      slideSpeed : 300,
-      paginationSpeed : 400,
+      slideSpeed : 800,
+      paginationSpeed : 800,
       singleItem:true,
       navigationText: false
- 
       // "singleItem:true" is a shortcut for:
       // items : 1, 
       // itemsDesktop : false,
