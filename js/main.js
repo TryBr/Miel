c155cd5e591676cd98a3d6a7c1125d1f05ce2269
TryBr <avulsed.jb@gmail.com>
2016-04-08 02:01:45 +0500

    Всё сверстано.

--- js/main.js
+++ js/main.js
@@ -24,3 +24,19 @@ $(document).ready(function() {
 $(document).ready(function() {
     $(".fancybox").fancybox();
   });
+
+$(document).ready(function() {
+  $('#vertical').lightSlider({
+    gallery:true,
+    item:1,
+    vertical:true,
+    verticalHeight:295,
+    vThumbWidth:145,
+    vThumbHeight:90,
+    thumbItem:3,
+    thumbMargin:4,
+    slideMargin:5,
+    loop: true,
+    speed: 600
+  });  
+});
