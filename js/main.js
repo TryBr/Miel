cd18e44b92d57b7757e9b7b0ea1184ff67234c18
TryBr <Avulsed.jb@gmail.com>
2016-04-07 16:39:26 +0500

    Менеджеры сделаны

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 $( "#tabs" ).tabs();
 $( "#tabs2" ).tabs();
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      navigationText: false
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
+});
